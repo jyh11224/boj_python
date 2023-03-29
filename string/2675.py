1625718da01f5a6587b91847c0841980f234cf9e
yeonghun <126738945+yeunghun@users.noreply.github.com>
2023-03-29 23:25:50 +0900

    Update 2675.py

--- string/2675.py
+++ string/2675.py
@@ -1,2 +1,12 @@
 # 백준 2675번 문제
 # 문제링크>> https://www.acmicpc.net/problem/2675
+t = int(input())
+
+for i in range(t):
+    r,s = input().split()
+    r = int(r)
+    s = str(s)
+    for j in s:
+        for k in range(r):
+            print(j, end = "")
+    print()
