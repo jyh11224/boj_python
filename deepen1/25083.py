37762ce7c03e2cdebb584c91570aa87061625fd3
yeonghun <126738945+yeunghun@users.noreply.github.com>
2023-04-01 22:58:10 +0900

    Create 25083.py

---
+++ deepen1/25083.py
@@ -0,0 +1,8 @@
+# 백준 25083번 문제
+# 문제링크>> https://www.acmicpc.net/problem/25083
+print("         ,r\'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r\'")
+print("   `~\\/")
+print("      |")
+print("      |")
