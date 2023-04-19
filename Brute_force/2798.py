7c59e171a8c450ba936f510c1fd7a2b22bb6076b
yeonghun <126738945+yeunghun@users.noreply.github.com>
2023-04-19 21:05:55 +0900

    Create 2798.py

---
+++ Brute_force/2798.py
@@ -0,0 +1,15 @@
+# 백준 2798번 문제
+# 문제링크>> https://www.acmicpc.net/problem/2798
+
+n,m = map(int, input().split())
+
+arr = list(map(int, input().split()))
+result = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if arr[i] + arr[j] + arr[k] > m:
+                continue
+            else:
+                result = max(result, arr[i]+arr[j]+arr[k])
+print(result)
